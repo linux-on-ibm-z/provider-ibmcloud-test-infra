863b72714df667ce3ca80cbae057af69c9207a19
vibhutisawant <Vibhuti.Sawant@ibm.com>
2025-09-10 22:31:40 +0530

    test

--- kubetest2-tf/data/vpc/vpc-instance/outputs.tf
+++ kubetest2-tf/data/vpc/vpc-instance/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = local.vpc_id
+  value = [local.vpc_id]
 }
 
 output "subnet_id" {
-  value = ibm_is_subnet.primary.id
+  value = [ibm_is_subnet.primary.id]
 }
 
 output "security_group_id" {
-  value = local.security_group
+  value = [local.security_group]
 }
