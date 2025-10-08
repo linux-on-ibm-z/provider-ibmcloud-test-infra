6503aef28366fd27a7d62a0abca09013c08471f9
Amulyam24 <amulmek1@in.ibm.com>
2025-10-08 17:25:50 +0530

    Improve logging for Boskos operations

--- hack/boskos.sh
+++ hack/boskos.sh
@@ -21,6 +21,11 @@ set -o pipefail
 USER=${USER:-"k8s-prow-job"}
 RESOURCE_TYPE=${RESOURCE_TYPE:-"powervs"}
 
+error() {
+    echo "$@" >&2
+    exit 1
+}
+
 trap 'cleanup ${HEART_BEAT_PID:-}' EXIT
 
 release_account(){
@@ -28,29 +33,28 @@ release_account(){
     status_code=$(curl -w '%{http_code}' -X POST ${url})
 
     if [[ ${status_code} != 200 ]]; then
-        echo "Failed to release resource: ${BOSKOS_RESOURCE_NAME}"
-        exit 1
-    fi 
+        error "Failed to release resource ${BOSKOS_RESOURCE_NAME} of type ${RESOURCE_TYPE} used by ${USER}"
+    fi
+
+    echo "Successfully released resource ${BOSKOS_RESOURCE_NAME} of type ${RESOURCE_TYPE}, previously used by ${USER}"
 }
 
 checkout_account(){
     resource_type=$1
-    set +o xtrace
+    set +o xtrace # Disable debug before checkout
     url="http://${BOSKOS_HOST}/acquire?type=${resource_type}&state=free&dest=busy&owner=${USER}"
     output=$(curl -X POST ${url})
     [ $? = 0 ] && status_code=200
 
     if [[ ${status_code} == 200 && ${output} =~ "failed" ]]; then
-        echo "Failed to acquire resource from Boskos of type ${RESOURCE_TYPE}"
-        exit 1
+        error "Failed to acquire free resource of type ${RESOURCE_TYPE}"
     elif [[ ${status_code} == 200 ]]; then
         export BOSKOS_RESOURCE_NAME=$(echo ${output} | jq -r '.name')
         export BOSKOS_REGION=$(echo ${output} | jq -r '.userdata["region"]')
         export BOSKOS_RESOURCE_ID=$(echo ${output} | jq -r '.userdata["service-instance-id"]')
         export BOSKOS_ZONE=$(echo ${output} | jq -r '.userdata["zone"]')
     else
-        echo "Failed to acquire resource due to invalid response, status code : ${status_code}"
-        exit 1
+        error "Failed to acquire free resource of type ${RESOURCE_TYPE} due to invalid response, status code : ${status_code}"
     fi
 }
 
@@ -61,10 +65,10 @@ heartbeat_account(){
     do
         status_code=$(curl -s -o /dev/null -w '%{http_code}' -X POST ${url})
         if [[ ${status_code} != 200 ]]; then
-            echo "Heart beat to resource '${BOSKOS_RESOURCE_NAME}' failed due to invalid response, status code: ${status_code}"
-            exit 1
+            error "Heart beat to resource '${BOSKOS_RESOURCE_NAME}' failed due to invalid response, status code: ${status_code}"
         fi
         count=$(( $count + 1 ))
+        echo "Resource ${BOSKOS_RESOURCE_NAME} of type ${RESOURCE_TYPE} is currently being used by ${USER}"
         sleep 60
     done
 }
@@ -90,6 +94,7 @@ checkout_account_status=$?
 # If checkout is successful, source the environment variables
 if [ "$checkout_account_status" -eq 0 ]; then
     source "${account_env_var_file}"
+    echo "Successfully acquired free resource ${BOSKOS_RESOURCE_NAME} of type ${RESOURCE_TYPE} by user ${USER}"
 else
     echo "Error getting account from Boskos" 1>&2
     rm -f "${account_env_var_file}"
